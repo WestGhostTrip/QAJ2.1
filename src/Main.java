a27921ea33b3f4afb37446ce305ce078247e4e60
Антон Илларионов <anton.illarionov1991@gmail.com>
2022-04-24 16:35:24 +0300

    QA2.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 1234; // стоимость билета
+        int bonusMile = 20; // бонус за мили
+
+        System.out.println((ticket / bonusMile) + " начисленных миль");
+    }
+}
